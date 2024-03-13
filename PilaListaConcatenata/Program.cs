e7bbfea3e0c4c5cbc10caa3be0f3292d09ab40d3
Lucas Corsi <corsi.lucasgabriel.studente@itispaleocapa.it>
2024-03-13 22:45:37 +0100

    Pila utilizzando logica Lista concatenata

--- PilaListaConcatenata/Program.cs
+++ PilaListaConcatenata/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,70 @@ using System.Threading.Tasks;
 
 namespace PilaListaConcatenata
 {
+    public class Nodo
+    {
+        public int Dato { get; set; }
+        public Nodo Successivo { get; set; }
+    }
+
+    public class Pila
+    {
+        private Nodo testa;
+
+        public Pila()
+        {
+            testa = null;
+        }
+
+        // PUSH
+        public void InserisciInTesta(int dato)
+        {
+            Nodo nuovoNodo = new Nodo { Dato = dato, Successivo = testa };
+            testa = nuovoNodo;
+        }
+
+        // POP
+        public bool CancellaInTesta()
+        {
+            if (testa != null)
+            {
+                //Nodo temp = testa;
+                testa = testa.Successivo;
+                //temp = null;
+                return true;
+            }
+            else
+                return false;
+        }
+        public override string ToString()
+        {
+            String s = "";
+            Nodo corrente = testa;
+            while(corrente != null)
+            {
+                s += corrente.Dato + "\n";
+                corrente = corrente.Successivo;
+            }
+            return s;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Pila pila = new Pila();
+
+            pila.InserisciInTesta(3);
+            pila.InserisciInTesta(2);
+            pila.InserisciInTesta(1);
+
+            Console.WriteLine(pila);
+
+            if (pila.CancellaInTesta())
+                Console.WriteLine(pila);
+            else
+                Console.WriteLine("La pila è vuota.");
         }
     }
 }
